81dc3ebe60d922e073f19543f1353e43f32be59b
Vitaly Domnikov <dotcypress@gmail.com>
2019-07-10 17:50:58 -0700

    add set method

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { InMemoryLRUCache, PrefixingKeyValueCache } = require('apollo-server-caching')
 
 class GenericDataSource {
-  constructor (keyPrefix = 'gencache:', ttl) {
+  constructor (keyPrefix = 'gen:', ttl) {
     this.keyPrefix = keyPrefix
     this.ttl = ttl
   }
@@ -14,18 +14,25 @@ class GenericDataSource {
 
   get (key, loaderFn, valueTTL) {
     return this.cache.get(key)
-      .then((value) => {
-        if (value) {
-          return JSON.parse(value)
-        }
-        return Promise.resolve(loaderFn())
-          .then((value) => {
-            const ttl = valueTTL || this.ttl
-            const options = ttl && { ttl }
-            return value && this.cache.set(key, JSON.stringify(value), options)
-              .then(() => value)
-          })
-      })
+      .then((raw) => raw
+        ? JSON.parse(raw)
+        : invalidate(key, loaderFn, valueTTL)
+      )
+  }
+
+  set (key, value, valueTTL) {
+    const ttlFn = valueTTL || this.ttl
+    const ttl = typeof ttlFn === 'function' ? ttlFn(value) : ttlFn
+    return this.cache.set(key, JSON.stringify(value), ttl && { ttl })
+      .then(() => value)
+  }
+
+  invalidate (key, loaderFn, valueTTL) {
+    return Promise.resolve(loaderFn())
+      .then((value) => value
+        ? this.set(key, value, valueTTL)
+        : value
+      )
   }
 }
 
